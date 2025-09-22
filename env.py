740c7ecd24cbdf0dbd608578b6fed8a5eadb5af5
Learan-Canning <learancanning@mail.com>
2025-09-22 12:13:21 +0100

    Update Settings

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_yhdV4BWm7TDZ@ep-frosty-wildflower-agnfsqkd.c-2.eu-central-1.aws.neon.tech/award_hurry_prank_799997")
+os.environ.setdefault("SECRET_KEY", "leonardo125gh!djsjsmd,sliajksksksksk")
